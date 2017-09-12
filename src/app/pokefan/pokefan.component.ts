0a2f971a29532b58f151d9b7ea98c52f062c44e5
Petter, Daniel (K-SIG/3) <daniel.petter@volkswagen.de>
2017-09-12 08:37:35 +0200

    add missed folders

---
+++ src/app/pokefan/pokefan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokefan',
+  templateUrl: './pokefan.component.html',
+  styleUrls: ['./pokefan.component.css']
+})
+export class PokefanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
